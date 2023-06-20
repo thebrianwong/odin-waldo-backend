866ca5d7fd171a45fb12cfeec1d10e5b273ad05e
Brian Wong <brianw7218@gmail.com>
2023-06-19 22:24:41 -0700

    Convert app file to TypeScript

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import createError from "http-errors";
+import express, { NextFunction, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+import ErrorResponse from "./types/errorResponse.type.js";
+
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404));
+});
+
+// error handler
+app.use(
+  (err: ErrorResponse, req: Request, res: Response, next: NextFunction) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  }
+);
+
+module.exports = app;
