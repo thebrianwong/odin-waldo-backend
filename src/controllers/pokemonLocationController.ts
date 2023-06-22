d3bb1c86c5beeb934dee6660978f0a559589078f
Brian Wong <brianw7218@gmail.com>
2023-06-21 21:39:43 -0700

    Stop queries from including the __v property in documents

--- src/controllers/pokemonLocationController.ts
+++ src/controllers/pokemonLocationController.ts
@@ -4,7 +4,7 @@ import PokemonLocationData from "../types/pokemonLocationData.type";
 
 const getPokemonLocationData: RequestHandler = async (req, res, next) => {
   const pokemonLocationData: Array<PokemonLocationData> =
-    await PokemonLocation.find({}, { _id: 0 });
+    await PokemonLocation.find({}, { _id: 0, __v: 0 });
   pokemonLocationData.sort((a, b) => {
     if (a.gameVersion < b.gameVersion) {
       return -1;
