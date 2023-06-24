3e2c426ced7821222b32c25d8c1c6428f334c90c
Brian Wong <brianw7218@gmail.com>
2023-06-23 22:16:48 -0700

    Create POST route to add new scores to the leaderboard

--- src/controllers/leaderboardController.ts
+++ src/controllers/leaderboardController.ts
@@ -1,4 +1,9 @@
-import { RequestHandler } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import {
+  FieldValidationError,
+  body,
+  validationResult,
+} from "express-validator";
 import Leaderboard from "../models/leaderboard.model";
 import LeaderboardEntry from "../types/leaderboardEntry.type";
 
@@ -23,4 +28,49 @@ const getLeaderboardEntries: RequestHandler = async (req, res, next) => {
   res.send(leaderboardEntries);
 };
 
-export { getLeaderboardEntries };
+const validateEntryDetails = [
+  body("name").trim().escape().default("Anonymous Trainer"),
+  body("score", "Your submission must include a score.").isNumeric().notEmpty(),
+  body("favoritePokemon").trim().escape().default("Missingno"),
+  body("timeStamp", "Your submission must include a valid date.")
+    .trim()
+    .escape()
+    .isISO8601()
+    .notEmpty(),
+  body("gameVersion", "Your submission must include a valid game version.")
+    .trim()
+    .escape()
+    .notEmpty()
+    .isIn(["version1", "version2", "version3"]),
+];
+
+const saveEntryToLeaderboard: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (errors.array().length) {
+    const errorMessages: { [key: string]: string } = {};
+    errors.array({ onlyFirstError: true }).map((error) => {
+      errorMessages[(error as FieldValidationError).path] = error.msg;
+    });
+    res.status(400).send({
+      success: false,
+      message: errorMessages,
+    });
+  } else {
+    const leaderboardEntryDetails = {
+      name: req.body.name,
+      score: req.body.score,
+      favoritePokemon: req.body.favoritePokemon,
+      timeStamp: req.body.timeStamp,
+      gameVersion: req.body.gameVersion,
+    };
+    const leaderboardEntry = new Leaderboard(leaderboardEntryDetails);
+    await leaderboardEntry.save();
+    res.status(201).send({ success: true });
+  }
+};
+
+export { getLeaderboardEntries, validateEntryDetails, saveEntryToLeaderboard };
--- src/routes/leaderboard.ts
+++ src/routes/leaderboard.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { getLeaderboardEntries } from "../controllers/leaderboardController";
+import {
+  getLeaderboardEntries,
+  saveEntryToLeaderboard,
+  validateEntryDetails,
+} from "../controllers/leaderboardController";
 
 const router = express.Router();
 
 // GET leaderboard sorted by best scores
 router.get("/", getLeaderboardEntries);
+
+// POST new score to leaderboard
+router.post("/new", validateEntryDetails, saveEntryToLeaderboard);
 
 export default router;
