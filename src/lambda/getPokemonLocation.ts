73f98f94754ef805c35743caf9f65a87afcf3005
Brian Wong <brianw7218@gmail.com>
2024-05-17 21:27:11 -0700

    Add error handling to location query function

--- src/lambda/getPokemonLocation.ts
+++ src/lambda/getPokemonLocation.ts
@@ -7,33 +7,36 @@ import { unmarshall } from "@aws-sdk/util-dynamodb";
 import PokemonLocationData from "../types/pokemonLocationData.type";
 
 export const getPokemonLocation = async () => {
-  const client = new DynamoDBClient();
-  const input: ScanInput = {
-    TableName: "pokemon-waldo",
-    ExpressionAttributeNames: {
-      "#T": "type",
-    },
-    ExpressionAttributeValues: {
-      ":type": {
-        S: "location",
+  try {
+    const client = new DynamoDBClient();
+    const input: ScanInput = {
+      TableName: "pokemon-waldo",
+      ExpressionAttributeNames: {
+        "#T": "type",
       },
-    },
-    FilterExpression: "#T = :type",
-  };
+      ExpressionAttributeValues: {
+        ":type": {
+          S: "location",
+        },
+      },
+      FilterExpression: "#T = :type",
+    };
+    const command = new ScanCommand(input);
+    const response = await client.send(command);
 
-  const command = new ScanCommand(input);
-  const response = await client.send(command);
+    const locationData = {
+      version1: {},
+      version2: {},
+      version3: {},
+    };
 
-  const locationData = {
-    version1: {},
-    version2: {},
-    version3: {},
-  };
+    response.Items?.forEach((item) => {
+      const parsedItem = unmarshall(item) as PokemonLocationData;
+      locationData[parsedItem.gameVersion] = parsedItem.data;
+    });
 
-  response.Items?.forEach((item) => {
-    const parsedItem = unmarshall(item) as PokemonLocationData;
-    locationData[parsedItem.gameVersion] = parsedItem.data;
-  });
-
-  return locationData;
+    return locationData;
+  } catch (error) {
+    throw Error(error as string);
+  }
 };
